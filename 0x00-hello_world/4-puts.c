8185bec0d8b34061c1062f3538b11708d26c4ed2
Tonie <victornwanochi@gmail.com>
2023-06-15 16:42:22 -0700

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints a string using put function
+ * Return: 0 (A return value of zero indicates success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
