148a56f66194c9fd7bd4edd122f9e56c8fb88c9f
Tonie <victornwanochi@gmail.com>
2023-06-15 16:52:23 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - A program that prints a string using printf
  * Return: 0 (return value of zero indicates success)
  */
-int main(void) :
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
