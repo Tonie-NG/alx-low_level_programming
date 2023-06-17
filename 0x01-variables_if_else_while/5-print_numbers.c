999ee19566124b0dd0e7545c7088facdbbbf2355
Tonie <victornwanochi@gmail.com>
2023-06-16 19:23:27 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point for number printing
+ * Return: 0 (indicates success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		printf(number);
+		number++;
+	}
+	printf('\n');
+	return (0);
+}
