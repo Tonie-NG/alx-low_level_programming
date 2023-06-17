b9012ae5d5692eed253f669eb88433375b51e58a
Tonie <victornwanochi@gmail.com>
2023-06-16 19:54:15 -0700

    Base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point hexadecimal numbers
+ * Return: 0 (indicates success)
+ */
+int main(void)
+{
+	char hexNumber = '0';
+
+	while (hexNumber <= f)
+	{
+		putchar(hexNumber);
+		hexNumber++;
+	}
+	putchar('\n');
+	return (0);
+}
