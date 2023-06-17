066bd203eba28fa19387f7ddbc8740d3d9489223
Tonie <victornwanochi@gmail.com>
2023-06-16 21:29:35 -0700

    Print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success indicator)
+ */
+int main(void)
+{
+	int number1 = 0;
+	int number2 = 1;
+
+	while (number1 < 10)
+	{
+		putchar('0' + digit1);
+		putchar('0' + digit2);
+
+		if (number1 < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		number2++;
+		if (number2 < 9)
+		{
+			number1++;
+			number2 = number1 + 1;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
