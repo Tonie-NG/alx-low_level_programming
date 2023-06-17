f22fa82be3880c423e018b62e340ec3047003f8e
Tonie <victornwanochi@gmail.com>
2023-06-16 19:18:03 -0700

    Print alphabets minus q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Alphabets except q and e
+ * Return: 0 (Indicates success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
