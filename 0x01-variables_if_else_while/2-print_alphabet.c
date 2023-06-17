25c8437f93993fb66927cd6648a6de6f83b867f1
Tonie <victornwanochi@gmail.com>
2023-06-16 18:38:57 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Program to print the alphabet
+ * Return: 0 (Indicates success)
+ */
+int main(void)
+{
+	char t;
+
+	t = 'a';
+	while
+		(t <= 'z') {
+			putchar(t);
+			t++;
+		}
+	putchar("\n");
+	return (0);
