1a76aaf4e7857faea61136c34dac1f9de7df4567
Tonie <victornwanochi@gmail.com>
2023-06-20 21:40:31 -0700

    Natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 always
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 1; x < 1024; x++)
+	{
+		if ((x % 3) == 0 || (x % 5) == 0)
+		{
+			b += a;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
+}
