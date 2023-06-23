4a597ae782084af2b5b6274f780ac093c7d0abac
Tonie <victornwanochi@gmail.com>
2023-06-22 19:51:37 -0700

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Fizz buzz program
+ * Return: 0
+ */
+int main(void)
+{
+	int v;
+
+	for (v = 1; v <= 100; v++)
+	{
+		if (v % 3 == 0 && v % 5 == 0)
+			printf("FizzBuzz");
+		else if (v % 3 == 0)
+			printf("Fizz");
+		else if (v % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", v);
+	}
+	printf("\n");
+	return (0);
+}
