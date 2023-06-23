86159a59d9d7af5bee0fe66a7c8fcda910ff6f39
Tonie <victornwanochi@gmail.com>
2023-06-22 19:00:52 -0700

    Is Digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 /**
  *  _isdigit - Checks if parameter is an integer between 0 and 9
  *  @c: input number
- *  Return - 1 on success or 0 if otherwise
+ *  Return: 1 on success or 0 if otherwise
  */
 int _isdigit(int c)
 {
