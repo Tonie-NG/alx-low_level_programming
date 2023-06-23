ef2b695a0a1545269fe5452855e59a9d3f37fbf9
Tonie <victornwanochi@gmail.com>
2023-06-22 19:19:11 -0700

    Print most numbers

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -1,19 +1,12 @@
 #include "main.h"
-/**
- * print_most_numbers - prints numbers between 0 to 9
- * (neither 2 nor 4).
- * Return: void
- */
-void print_most_numbers(void)
-{
-	int vh;
 
-	for (vh = 48; vh < 58; vh++)
-	{
-		if (vh != 50 && vh != 52)
-		{
-			_putchar(vh);
-		}
-		_putchar('\n');
-	}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_most_numbers();
+    return (0);
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers between 0 to 9
+ * (neither 2 nor 4).
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int vh;
+
+	for (vh = 48; vh < 58; vh++)
+	{
+		if (vh != 50 && vh != 52)
+		{
+			_putchar(vh);
+		}
+	}
+	_putchar('\n');
+}
