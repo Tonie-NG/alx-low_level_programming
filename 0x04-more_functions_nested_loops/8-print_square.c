82b13c72c3ddd975074d72090de72caba87ad31d
Tonie <victornwanochi@gmail.com>
2023-06-22 19:40:58 -0700

    Print sqaure

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - Prints square hashes
+ * @size: size of the square.
+ * Return: void
+ */
+void print_square(int size)
+{
+	int v t;
+
+	for (v = 0; v < size; v++)
+	{
+		for (t =0; t < size; t++)
+		{
+			_putchar(35);
+		}
+		if (v != size -1)
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
