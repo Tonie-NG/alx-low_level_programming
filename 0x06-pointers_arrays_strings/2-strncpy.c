bed14eec0196b27c4a51520628e883a93c467c43
Tonie <victornwanochi@gmail.com>
2023-06-29 18:33:28 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,7 +16,7 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	for (; v < n; v++)
 	{
-		dset[v] = '\0';
+		dest[v] = '\0';
 	}
 	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _strcmp - compares string values
+ * @s1: input value
+ * @s2: input value
+ * Return: s1[i] - s2[i]
+ */
+
