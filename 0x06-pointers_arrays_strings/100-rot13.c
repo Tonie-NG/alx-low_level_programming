c8d358ea258ad50a62aaa40ab88adb3aae8b5ea1
Tonie <victornwanochi@gmail.com>
2023-06-29 19:14:20 -0700

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,16 +6,16 @@
  */
 char *rot13(char *s)
 {
-	vnt v, t;
+	int v, t;
 
-	char *x = "abcdefghvtklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *y = "nopqrstuvwxyzabcdefghvtklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (v = 0; s[v] != '\0'; v++)
 	{
 		for (t = 0; x[t] != '\0'; t++)
 		{
-			vf (s[v] == x[t])
+			if (s[v] == x[t])
 			{
 				s[v] = y[t];
 				break;
