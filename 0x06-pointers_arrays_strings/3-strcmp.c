b442b900a6f2bfa6bd9469e35d4315ebe58a2834
Tonie <victornwanochi@gmail.com>
2023-06-29 18:37:26 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,4 +5,18 @@
  * @s2: input value
  * Return: s1[i] - s2[i]
  */
+int _strcmp(char *s1, char *s2)
+{
+	int v;
 
+	v = 0;
+	while (s1[v] != '\0' && s2[v] != '\0')
+	{
+		if (s1[v] != s2[v])
+		{
+			return (s1[v] - s2[v]);
+		}
+		v++;
+	}
+	return (0);
+}
