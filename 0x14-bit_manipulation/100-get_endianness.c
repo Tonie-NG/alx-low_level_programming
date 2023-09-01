d29516323b97c2f2a611c372548c86c9290fed52
Tonie Victor <victornwanochi@gmail.com>
2023-09-01 06:55:07 -0700

    solved task 6 on bit manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,9 +1,14 @@
 #include "main.h"
+/**
+ * get_endianness - function that checks the endianness
+ * of a machine
+ * Return: 1 if Little Endian or 0 if not
+ */
 
 int get_endianness(void)
 {
 	unsigned int n = 1;
-	char *byte_ptr = (char *) & n;
+	char *byte_ptr = (char *) &in;
 
 	if (*byte_ptr == 1)
 		return (1);
