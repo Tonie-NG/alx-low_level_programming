58232985c751a2a3bc17e4791af11782bb26dac1
Tonie Victor <victornwanochi@gmail.com>
2023-09-01 06:09:31 -0700

    solved task 5 on bit manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - unction that returns the number of bits
+ * you would need to flip to get from one number to another.
+ * @m: first number
+ * @m: second number
+ * Return: the number of nits to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int result = n ^ m;
+	unsigned int i = 0;
+
+	while (result)
+	{
+		i += result & 1;
+		result >>= 1;
+	}
+
+	return (i);
+}
