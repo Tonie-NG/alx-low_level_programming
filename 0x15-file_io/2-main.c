7a68d58a355227e24207632c459ce091a6caa144
Tonie Victor <victornwanochi@gmail.com>
2023-10-02 04:52:39 -0700

    Done with the tests

---
+++ 0x15-file_io/2-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @ac: number of arguments
+ * @av: an array that holds the number of arguments
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	int res;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
+	res = append_text_to_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
+}
+
