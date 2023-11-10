63f52988a55e809b668781c901cb75be69aa57f5
Tonie Victor <victornwanochi@gmail.com>
2023-11-09 19:44:37 -0800

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
@@ -9,86 +10,39 @@
  *
  * Return: pointer to the new node, or NULL on failure
  */
-
-unsigned int dt_len(dlistint_t *h);
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *current_head, *temp;
-	unsigned int i = dt_len(*h), p = 0;
+	dlistint_t *new, *next, *current;
+	unsigned int i;
 
-	if (idx > i)
-	{
+	if (h == NULL)
 		return (NULL);
-	}
-	current_head = *h;
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (idx != 0)
 	{
+		current = *h;
+		for (i = 0; i < idx - 1 && current != NULL; i++)
+			current = current->next;
+		if (current == NULL)
+			return (NULL);
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
-	}
-	new_node->n = n;
-	if (current_head == NULL)
-	{
-		new_node->prev = NULL;
-		new_node->next = NULL;
-		current_head = new_node;
-		return (new_node);
-	}
-
+	new->n = n;
 	if (idx == 0)
 	{
-		new_node->prev = NULL;
-		new_node->next = *h;
-		*h = new_node;
-
-		if (new_node->next != NULL)
-		{
-			(new_node->next)->prev = new_node;
-		}
-		return (new_node);
+		next = *h;
+		*h = new;
+		new->prev = NULL;
 	}
-
-	if (idx == i - 1)
+	else
 	{
-		new_node->next = NULL;
-		while (current_head->next != NULL)
-			current_head = current_head->next;
-
-		new_node->prev = current_head;
-		current_head->next = new_node;
-		return (new_node);
+		new->prev = current;
+		next = current->next;
+		current->next = new;
 	}
-
-	temp = *h;
-	while (p < idx - 1)
-	{
-		temp = temp->next;
-		p++;
-	}
-	new_node->prev = temp;
-	new_node->next = temp->next;
-	temp->next = new_node;
-
-	return (new_node);
+	new->next = next;
+	if (new->next != NULL)
+		new->next->prev = new;
+	return (new);
 }
-
-/**
- * dt_len - function that returns the number of
- * elements in a linked dlistint_t list.
- * @h: pointer to the first node
- *
- * Return: number of nodes
- */
-unsigned int dt_len(dlistint_t *h)
-{
-	unsigned int i = 0;
-
-	while (h != NULL)
-	{
-		h = h->next;
-		i++;
-	}
-	return (i);
-}
-
